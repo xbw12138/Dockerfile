f4fc1d44c8a9cdd19d887e16afdb54d0ca4d0dec
BadApple <1076351865@qq.com>
2017-04-25 00:23:02 +0800

    init

---
+++ ss.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Auth:XBW
+# Email:xbw@ecfun.cc
+# QQ:1076351865
+#
+#
+echo "棒棒 xbw@ecfun.cc 新浪微博 @DMT许博文"
+/etc/init.d/shadowsocks restart &&
+service crond start ; /usr/sbin/sshd -D
+
+
